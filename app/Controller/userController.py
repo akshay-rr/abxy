6a1bf983ac117ba88e7cf593811dff6fb32bf0ee
akshay-rr <akshayrajagopal10@gmail.com>
2020-07-15 01:36:24 +0530

    fingers crossed

--- app/Controller/userController.py
+++ app/Controller/userController.py
@@ -6,13 +6,13 @@ class UserController:
 	def __init__(self, tdb: TaskDatabase):
 		self.taskDatabase = tdb
 
-	def createUser(self, user: User):
+	def createUser(self, user: User) -> int:
 		if self.taskDatabase.getUserIDByEmail(user.email) != -1:
 			# the user already exists!
 			return -1
 		return self.taskDatabase.putNewUser(user)
 
-	def loginUser(self, user: User):
+	def loginUser(self, user: User) -> int:
 		uid = self.taskDatabase.getUserIDByEmail(user.email)
 		if uid == -1:
 			return -1
