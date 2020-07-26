e899caee7a0a2c3f3ebe718737beef640cce793a
Abhinav Ramachandran <geckods@gmail.com>
2020-07-26 16:36:11 +0530

    Added repeat bonus repetition calculation logic.

--- app/Controller/bonusController.py
+++ app/Controller/bonusController.py
@@ -9,6 +9,70 @@ class BonusController:
 	def __init__(self, tdb: TaskDatabase):
 		self.taskDatabase = tdb
 
+	@staticmethod
+	def computeScoreAddition(bonus, data):
+		constants = bonus['constants']
+		if bonus['evaluation_type'] == "ADDITIVE":
+			return round(constants[0] * data)
+		elif bonus['evaluation_type'] == "SIGMOID":
+			return round(constants[0] * math.tanh(data / (constants[1] / 2)))
+
+	@staticmethod
+	def hourDistanceIsOne(early: datetime, late: datetime):
+		if early.date() == late.date():
+			return late.hour - early.hour <= 1 and early.hour <= late.hour
+
+		delta = late.date() - early.date()
+		if delta.days == 1:
+			return early.hour == 23 and late.hour == 0
+
+		return False
+
+	@staticmethod
+	def dateDistanceIsOne(early: datetime, late: datetime):
+		delta = late.date() - early.date()
+		return 1 >= delta.days >= 0
+
+	@staticmethod
+	def isoYearHas53Weeks(isoYear: int):
+		g = lambda y: math.floor((y - 100) / 400) - math.floor((y - 102) / 400)
+		h = lambda y: math.floor((y - 200) / 400) - math.floor((y - 199) / 400)
+		f = lambda y: 5 * y + 12 - 4 * (math.floor(y / 100) - math.floor(y / 400)) + g(y) + h(y)
+		return f(isoYear) < 5
+
+	@staticmethod
+	def weekDistanceIsOne(early: datetime, late: datetime):
+		earlyiso = early.isocalendar()
+		lateiso = late.isocalendar()
+		if earlyiso[0] == lateiso[0]:
+			return lateiso[1] - earlyiso[1] <= 1 and earlyiso[1] <= lateiso[1]
+		elif earlyiso[0] == (lateiso[0] - 1):
+			return lateiso[1] == 1 and earlyiso[1] == 52 + (BonusController.isoYearHas53Weeks(earlyiso[0]))
+
+	@staticmethod
+	def monthDistanceIsOne(early: datetime, late: datetime):
+		return (early.year == late.year and early.month <= late.month and late.month - early.month <= 1) or (
+				early.year == (late.year - 1) and early.month == 12 and late.month == 1)
+
+	@staticmethod
+	def yearDistanceIsOne(early: datetime, late: datetime):
+		return late.year >= early.year and late.year - early.year <= 1
+
+	@staticmethod
+	def isTimeRepeat(oldTime, newTime, frequency):
+		if frequency == "HOURLY":
+			return BonusController.hourDistanceIsOne(oldTime, newTime)
+		elif frequency == "DAILY":
+			return BonusController.dateDistanceIsOne(oldTime, newTime)
+		elif frequency == "WEEKLY":
+			return BonusController.weekDistanceIsOne(oldTime, newTime)
+		elif frequency == "MONTHLY":
+			return BonusController.monthDistanceIsOne(oldTime, newTime)
+		elif frequency == "YEARLY":
+			return BonusController.yearDistanceIsOne(oldTime, newTime)
+		else:
+			return False
+
 	def createBonus(self, bonusCreationRequest):
 		bonusObject = {}
 		uid = bonusCreationRequest['uid']
@@ -25,17 +89,14 @@ class BonusController:
 
 	def getDataQuantity(self, uid, task_id, bonus):
 		if bonus['data_source'] == "REPETITION":
+			timeNow = datetime.now()
 			# get the most recent taskLog with the same uid,tid
 			mostRecentLog = self.taskDatabase.getMostRecentLogByUserIDAndTaskID(uid, task_id)
 			for bonusInstance in mostRecentLog['bonus_instances']:
 				# check the input value for the same bonus
 				if bonusInstance['bonus_id'] == bonus['_id']:
-					return float(bonusInstance['input_quantity'] + 1)
+					if BonusController.isTimeRepeat(mostRecentLog['timestamp'], timeNow, bonus['input_label']):
+						return float(bonusInstance['input_quantity'] + 1)
+					else:
+						return float(bonusInstance['input_quantity'])
 		return None
-
-	def computeScoreAddition(self, bonus, data):
-		constants = bonus['constants']
-		if bonus['evaluation_type'] == "ADDITIVE":
-			return round(constants[0] * data)
-		elif bonus['evaluation_type'] == "SIGMOID":
-			return round(constants[0] * math.tanh(data / (constants[1] / 2)))
